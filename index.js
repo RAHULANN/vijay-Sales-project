6adcc277ee56a3c9ec70030c25bb9ed93111471b
riteshjk <96072567+riteshjk@users.noreply.github.com>
2022-02-25 12:09:50 +0530

    Add files via upload
    adding some changes

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+
+const connect = require('./configs/db');
+const userController = require("./controller/user.controller")
+const productController = require("./controller/product.controller")
+const passport = require("./configs/google-oauth")
+
+const {register,login,newToken} = require("./controller/auth.controller")
+
+app.use(express.json())
+app.use("/users", userController)
+app.use("/products", productController)
+
+app.post("/register",register)
+app.post("/login",login)
+
+
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+  	[ 'email', 'profile' ] }
+));
+ 
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        //successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+}),(req, res) => {
+    const token = newToken(req.user)
+
+
+        res.send({user:req.user, token})
+    return res.send(req.user)
+});
+
+passport.serializeUser(function(user, done) {
+    console.log("5",user)
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    console.log("25",user)
+
+    done(null, user);
+  });
+
+
+app.listen(2233,async()=>{
+    await connect();
+    console.log("listening on port 2233")
+})
+
+
+
+
+
+
