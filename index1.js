dcd5135a4ec62ff4d72d63030ce5db0c41219255
riteshjk <96072567+riteshjk@users.noreply.github.com>
2022-02-26 17:15:51 +0530

    some changes

--- index1.js
+++ index1.js
@@ -22,7 +22,7 @@ app.get('/auth/google',
  
 app.get( '/auth/google/callback',
     passport.authenticate( 'google', {
-      //  successRedirect: '/homepage.html',
+        successRedirect: '/auth/google/index.html',
         failureRedirect: '/auth/google/failure'
 }),(req, res) => {
     const token = newToken(req.user)
